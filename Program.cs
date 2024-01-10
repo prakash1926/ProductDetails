9415985d1562c760a8a36952a618910fe72cc28e
prakash1926 <ananthaprakash71@gmail.com>
2024-01-10 15:03:23 +0530

    Insertion

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ List<Products> product = new List<Products>()
     new Products() {PName="Samsung S32",PPrice=86000,PBrand="Samsung",ManufacturingDate=new DateTime(day:10,month:08,year:2020),ExpiryDate=new DateTime(day:12,month:07,year:2024)},
     new Products() {PName="Redmi note12",PPrice=75000,PBrand="Mi",ManufacturingDate=new DateTime(day:15,month:07,year:2021),ExpiryDate=new DateTime(day:22,month:05,year:2022)},
     new Products() {PName="iPhone x13",PPrice=89000,PBrand="Apple",ManufacturingDate=new DateTime(day:12,month:09,year:2021),ExpiryDate=new DateTime(day:15,month:05,year:2022)},
+    new Products() {PName="Vivo y35",PPrice=89000,PBrand="Vivo",ManufacturingDate=new DateTime(day:17,month:07,year:2021),ExpiryDate=new DateTime(day:19,month:09,year:2025)},
   
 };
 Console.WriteLine("PName\t \t PPrice \t PBrand \t MfDate \t \t ExpDate");
